893588e8e09ed6c6a99bb0981184501cce05840f
Frances <frances.ferrer1999@gmail.com>
2022-12-03 15:42:00 +0100

    first commit. TODO: stretch goal

---
+++ index.js
@@ -0,0 +1,39 @@
+const seedColorEl = document.getElementById('seed-color')
+const selectEl = document.getElementById('mode-selector')
+const form = document.getElementById('form')
+const colorScheme = document.getElementById('color-scheme')
+
+let colorsArray = []
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let seed = seedColorEl.value.slice(1)
+    let mode = selectEl.value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seed}&mode=${mode}&count=5`)
+        .then(res => res.json())
+        .then(data => {
+                colorsArray = data.colors
+                renderColors()
+            }
+        )
+})
+
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.hexValue){
+        console.log(e.target)
+        //navigator.clipboard.writeText(e.target.dataset.hexValue)
+    }
+})
+
+function renderColors(){
+    let colorsHtml = ''
+    for(let color of colorsArray){
+        let hexValue = color.hex.value
+        colorsHtml += `
+            <div class="color" style="background-color: ${hexValue}">
+                <button class="hex-value-btn btn" data-hexValue="${hexValue}">${hexValue}</button>
+            </div>
+        `
+    }
+    colorScheme.innerHTML = colorsHtml
+}
